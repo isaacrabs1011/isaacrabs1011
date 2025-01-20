50b7010627003809170f609abd759554bc1e1fd0
isaacrabs <119681538+isaacrabs@users.noreply.github.com>
2025-01-20 11:26:07 +0000

    Integrated spotipy playlist stuff into the CLI

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import random
-# import numpy as np
-# import matplotlib as mpl
-# import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 #
-# import spotipy
-# from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 # SPOTIPY_CLIENT_ID = '5143755320f74541986016c151d8b004'
 # SPOTIPY_CLIENT_SECRET = '8466dcc0498847eabf4cc3b83b6a0742'
@@ -106,18 +106,17 @@ class Game:
         """
         This playlist will be replaced by the user's spotify playlist. This is where I'd integrate the Spotify API
         """
-        self.playlist = [
-            "Noid",
-            "Bennie and The Jets",
-            "Mojo Pin",
-            "Daughter of a Cop",
-            "Reincarnated",
-            "Happy",
-            "Feather",
-            "Mercury",
-            "06 Dreamin",
-            "Nothing's Promised"
-        ]
+        self.playlist = []
+        SPOTIPY_CLIENT_ID = '5143755320f74541986016c151d8b004'
+        SPOTIPY_CLIENT_SECRET = '8466dcc0498847eabf4cc3b83b6a0742'
+        # SPOTIPY_REDIRECT_URI = 'http://localhost.callback'
+
+        auth_manager = SpotifyClientCredentials(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
+        sp = spotipy.Spotify(auth_manager=auth_manager)
+        playlist = sp.playlist('https://open.spotify.com/playlist/6GhmUa261lqAmWsod8A6vu?si=4d354f348327435b')
+        for item in playlist['tracks']['items']:
+            track = item['track']
+            self.playlist.append(track['artists'][0]['name'] + ' - ' + track['name'])
 
     def displayPlaylist(self):
         """
@@ -129,16 +128,7 @@ class Game:
             print(f'{count}: {item}')
             count += 1
 
-        # SPOTIPY_CLIENT_ID = '5143755320f74541986016c151d8b004'
-        # SPOTIPY_CLIENT_SECRET = '8466dcc0498847eabf4cc3b83b6a0742'
-        # # SPOTIPY_REDIRECT_URI = 'http://localhost.callback'
-        #
-        # auth_manager = SpotifyClientCredentials(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
-        # sp = spotipy.Spotify(auth_manager=auth_manager)
-        # playlist = sp.playlist('https://open.spotify.com/playlist/6GhmUa261lqAmWsod8A6vu?si=4d354f348327435b')
-        # for item in playlist['tracks']['items']:
-        #     track = item['track']
-        #     print(track['artists'][0]['name'] + ' - ' + track['name'])
+
 
     def setup(self):
         """
